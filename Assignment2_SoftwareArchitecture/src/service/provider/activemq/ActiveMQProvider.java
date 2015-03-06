28954f791a6f2804eaf60c1f9de07bd34cfc2230
forskan <forskan@gmail.com>
2015-03-06 12:06:17 +0100

    start

---
+++ Assignment2_SoftwareArchitecture/src/service/provider/activemq/ActiveMQProvider.java
@@ -0,0 +1,26 @@
+package service.provider.activemq;
+
+import service.provider.MessageReceiver;
+import service.provider.ServiceProvider;
+
+public class ActiveMQProvider implements ServiceProvider {
+
+	@Override
+	public void startListening(String endPoint, MessageReceiver messageReceiver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopListening() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendMessage(String msgText, String destinationEndPoint) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
